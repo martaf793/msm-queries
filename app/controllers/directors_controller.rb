a482b12d566534782fe0984d9307e8c1496faa1b
martaf793 <martaf@uchicago.edu>
2025-04-28 23:55:28 +0000

    details

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -3,6 +3,9 @@ class DirectorsController < ApplicationController
     render({:template=> "director_templates/list"})
   end 
   def show
+    @the_id=params.fetch("the_id")
+    @matching_rec=Director.where({:id=> the_id})
+    @the_director=@matching_rec.at(0)
     render({:template => "director_templates/details"})
   end 
 end
