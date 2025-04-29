d0fa1613bd54c74c37c82a4f23b622c4b3cd8918
martaf793 <martaf@uchicago.edu>
2025-04-29 03:49:28 +0000

    moviesok

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,9 +3,9 @@ class MoviesController < ApplicationController
     render({:template=> "movies_templates/list"})
   end 
   def show
-    @the_id=params.fetch("the_id")
-    @matching_rec=Movie.where({:id=> the_id})
-    @the_movie=@matching_rec.at(0)
+    the_id=params.fetch("the_id")
+    matching_rec=Movie.where({:id=> the_id})
+    @the_movie=matching_rec.at(0)
     render({:template => "movies_templates/details"})
   end 
 end
