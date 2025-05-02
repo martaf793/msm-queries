ed694ded0d6a843aaac7316354b2f4ed12283ce1
martaf793 <martaf@uchicago.edu>
2025-05-02 15:55:07 +0000

    actors

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -4,8 +4,8 @@ class ActorsController < ApplicationController
   end 
   def show
     @the_id=params.fetch("the_id")
-    @matching_rec=Director.where({:id=> the_id})
-    @the_director=@matching_rec.at(0)
+    @matching_rec=Actor.where({:id=> the_id})
+    @the_actor=@matching_rec.at(0)
     render({:template => "actor_templates/details"})
   end 
 end
